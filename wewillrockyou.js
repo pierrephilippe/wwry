78da0b54960d40f5aa42cc452d70b5c134e1c01e
Pierre-Philippe Fady <piefady@publicisgroupe.net>
2020-03-11 11:19:38 +0100

    add librarie wx

--- wewillrockyou.js
+++ wewillrockyou.js
@@ -61,7 +61,10 @@ $( document ).ready(function() {
     play.show();
   });
 
-  $( document ).updateAppMessageShareData({
+});
+
+wx.ready(function () {   // This must be called before the user potentially taps the **Share** button.
+  wx.updateAppMessageShareData({
     title: 'We Will Rock You on WeChat', // The sharing title
     desc: 'Test de pifi pour wechat', // The sharing description
     link: 'https://pifi.netlify.com', // The sharing link whose domain name or path must be consistent with the JS secure domain name of the Official Account corresponding to the current page.
